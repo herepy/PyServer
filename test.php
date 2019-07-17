ead9dc2a95e4ce28f93fbdbd6bfc4754dc45db34
Pengyu <917103429@qq.com>
2019-07-17 21:29:45 +0800

    自动加载helper助手文件

--- test.php
+++ test.php
@@ -10,4 +10,5 @@ require_once "vendor/autoload.php";
 
 use PyServer\Worker\MasterWorker;
 
-$worker=new MasterWorker("hTTp://0.0.0.0:8080");
+$worker=new MasterWorker("http://0.0.0.0:8080");
+$worker->run();
