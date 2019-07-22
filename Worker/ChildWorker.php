8e73b8c7fad5c8038c83c67dcee501d38b9966e4
pengyu <917103429@qq.com>
2019-07-22 11:54:47 +0800

    工作进程自行创建监听socket，判断是否需要端口复用

--- Worker/ChildWorker.php
+++ Worker/ChildWorker.php
@@ -8,6 +8,7 @@
 
 namespace PyServer\Worker;
 
+
 use PyServer\Scheduler\Event;
 
 class ChildWorker implements WorkerInterface
@@ -27,10 +28,34 @@ class ChildWorker implements WorkerInterface
      */
     protected $scheduler;
 
-    public function __construct($socket)
+    public function __construct($transport,$address,$port)
     {
-        $this->socket=$socket;
         $this->id=spl_object_hash($this);
+
+        //todo 记录
+        echo "listen:".$address." port:".$port."\n";
+        //创建监听socket
+        $domain=$transport == "unix"?AF_UNIX:AF_INET;
+        $type=$transport == "tcp"?SOCK_STREAM:SOCK_DGRAM;
+
+        if ($transport == "unix") {
+            $protocol=0;
+        }else{
+            $protocol=getprotobyname($transport);
+        }
+
+        //创建监听socket
+        $this->socket=socket_create($domain,$type,$protocol);
+        if (!$this->socket) {
+            die("create socket failed");
+        }
+
+        //不是unix,设置端口复用
+        if ($protocol !== 0) {
+            socket_set_option($this->socket,SOL_SOCKET,SO_REUSEPORT,1);
+        }
+        socket_bind($this->socket,$address,$port);
+        socket_listen($this->socket);
     }
 
     public function on($event, $callback)
@@ -45,12 +70,17 @@ class ChildWorker implements WorkerInterface
 
     public function run()
     {
+        //todo 目前只有event调度器
+        $this->scheduler=new Event();
+        $this->scheduler->init();
+        $this->scheduler->add($this->socket,Event::TYPE_READ,[$this,"accept"],$this->socket);
+        $this->scheduler->loop();
     }
 
     public function accept($socket)
     {
         $con=socket_accept($socket);
-        echo "connected:".$con;
+        echo "worker:{$this->id} get connected:".$con."\n";
     }
 
 }
