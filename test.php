62ef6070f439f56957d06f32c86c888aafd1ee6a
pengyu <917103429@qq.com>
2019-07-22 11:54:59 +0800

    设置进程数测试

--- test.php
+++ test.php
@@ -11,27 +11,5 @@ require_once "vendor/autoload.php";
 use PyServer\Worker\MasterWorker;
 
 $worker=new MasterWorker("http://127.0.0.1:8080");
+$worker->config(["workerCount"=>2]);
 $worker->run();
-
-
-//=====================================================================
-
-//$socket=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-//if (!$socket) {
-//    die("create fail");
-//}
-////socket_set_option($socket,SOL_SOCKET,SO_REUSEPORT,1);
-//
-//socket_bind($socket,"127.0.0.1","8888");
-//socket_listen($socket);
-//
-//$event_base=new \EventBase();
-//$event=new \Event($event_base,$socket,\Event::READ,"connect_cb",$socket);
-//$event->add();
-//$event_base->loop();
-//echo "end";
-//function connect_cb($socket)
-//{
-//    $con=socket_accept($socket);
-//    echo "client connected:".$con;
-//}
