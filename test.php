3a26d640e77ae57df167b2bf5f5b8effc4148272
pengyu <917103429@qq.com>
2019-07-23 17:48:30 +0800

    测试onMessage回调

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ use PyServer\Worker\MasterWorker;
 
 $worker=new MasterWorker("http://127.0.0.1:8080");
 //$worker->config(["workerCount"=>2]);
-$worker->on('connect',function (\PyServer\Transport\TransportInterface $transport,$fd){
-    echo "client connected:".intval($fd)."\n";
+$worker->on('message',function (\PyServer\Transport\TransportInterface $transport,$fd,$content){
+    $transport->send($fd,"hello world");
 });
 $worker->run();
