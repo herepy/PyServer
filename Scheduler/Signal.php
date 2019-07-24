2d69a059c8bddcaf1baac507916ad7d22933685a
Pengyu <917103429@qq.com>
2019-07-24 21:53:49 +0800

    完善属性

--- Scheduler/Signal.php
+++ Scheduler/Signal.php
@@ -12,7 +12,7 @@ namespace PyServer\Scheduler;
 class Signal implements SchedulerInterface
 {
     /**
-     * @var array 一次性定时事件
+     * @var array 一次性定时事件 [id => [timeStamp=>xxx,callback=>xxx]]
      */
     protected $onceTimer=[];
 
@@ -20,6 +20,11 @@ class Signal implements SchedulerInterface
      * @var array 定期执行事件
      */
     protected $timer=[];
+
+    /**
+     * @var int 全局定时器自增id（是新定时器的id，然后自增一）
+     */
+    public static $timerId;
 
     public function init()
     {
