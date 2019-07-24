a9c7ff38f096b1c297e67b20996576709f6761f0
pengyu <917103429@qq.com>
2019-07-24 11:20:28 +0800

    解析获取http报文大小

--- Protocol/Http.php
+++ Protocol/Http.php
@@ -12,22 +12,44 @@ class Http implements ProtocolInterface
 {
 
     protected $transport;
+    public static $allowMethods=["GET","POST","HEAD","DELETE","OPTIONS","PUT"];
 
     public static function size($buffer)
     {
-        // TODO: Implement size() method.
-        return strlen(substr($buffer,5));
+        //验证格式
+        $tmp=explode("\r\n\r\n",$buffer,2);
+        $header=$tmp[0];
+        $headerArr=explode("\r\n",$header);
+        if (!count($headerArr)) {
+            return 0;
+        }
+
+        //解析请求首行
+        $first=explode(" ",$headerArr[0]);
+        if (count($first) != 3) {
+            return 0;
+        }
+
+        //验证请求方法
+        if (!in_array($first[0],self::$allowMethods)) {
+            return 0;
+        }
+
+        //没有负载的请求
+        if ($first[0] == "GET" || $first[0] == "HEAD" || $first[0] == "DELETE" || $first[0] == "OPTIONS") {
+            return strlen($header)+strlen("\r\n\r\n");
+        }
+
+        //有负载的,从Content-Length中获取负载大小
+        if (preg_match("/Content-Length: ?(\d+)/",$header,$matches)) {
+            return strlen($header)+strlen("\r\n\r\n")+$matches[1];
+        }
+        return 0;
     }
 
     public static function decode($buffer,$size)
     {
-        // TODO: Implement decode() method.
-        $content=substr($buffer,5);
-
-        if (strlen($content) != $size) {
-            return false;
-        }
-        return $content;
+        
     }
 
     public static function encode($content)
