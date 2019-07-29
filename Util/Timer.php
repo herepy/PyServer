7852d168d425cf4bcb8ec2490e28c777dae9848a
pengyu <917103429@qq.com>
2019-07-29 12:09:39 +0800

    定时器简化重构

--- Util/Timer.php
+++ Util/Timer.php
@@ -11,20 +11,11 @@ namespace PyServer\Util;
 use PyServer\Scheduler\SchedulerInterface;
 use PyServer\Scheduler\Event;
 use PyServer\Scheduler\Signal;
+use PyServer\Worker\ChildWorker;
 
 class Timer
 {
     protected $id;
-
-    /**
-     * @var SchedulerInterface 调度器实例
-     */
-    protected static $scheduler;
-
-    /**
-     * @var float 定时器秒数
-     */
-    protected $seconds;
 
     /**
      * @var bool 是否持续化
@@ -42,57 +33,13 @@ class Timer
      * @param float $seconds 秒数
      * @param callable $callback 回调函数
      * @param bool $persist 是否持续化
-     * @param SchedulerInterface $scheduler 使用的调度器
      */
-    public function __construct($seconds,$callback,$persist=false,$scheduler=null)
+    public function __construct($seconds,$callback,$persist=false)
     {
-        if (!self::$scheduler) {
-            $this->init($scheduler);
-        }
-
-        $this->seconds=$seconds;
-        $this->callback=$callback;
         $this->persist=$persist;
-    }
-
-    /**
-     * 初始化调度器
-     * @param SchedulerInterface $scheduler
-     */
-    protected function init($scheduler=null)
-    {
-        self::$scheduler=new Signal();
-        if ($scheduler instanceof SchedulerInterface) {
-            self::$scheduler=$scheduler;
-            return;
-        }
-        if (extension_loaded("event") && class_exists("\PyServer\Scheduler\Event")) {
-            self::$scheduler=new Event();
-        } else {
-            self::$scheduler=new Signal();
-        }
-
-        self::$scheduler->init();
-    }
-
-    /**
-     * 设置回调函数
-     * @param callable $callback
-     */
-    public function setCallBack(callable $callback)
-    {
-        $this->callback=$callback;
-    }
-
-    /**
-     * 开始执行定时器
-     */
-    public function start()
-    {
-        $type=$this->persist?SchedulerInterface::TYPE_TIMER:SchedulerInterface::TYPE_ONCE_TIMER;
-        $timerId=self::$scheduler->add($this->seconds,$type,$this->callback);
+        $type=$persist?SchedulerInterface::TYPE_TIMER:SchedulerInterface::TYPE_ONCE_TIMER;
+        $timerId=ChildWorker::$scheduler->add($seconds,$type,$callback);
         $this->id=$timerId;
-        self::$scheduler->loop();
     }
 
     /**
@@ -104,7 +51,7 @@ class Timer
             return false;
         }
         $type=$this->persist?SchedulerInterface::TYPE_TIMER:SchedulerInterface::TYPE_ONCE_TIMER;
-        return self::$scheduler->del($this->id,$type);
+        return ChildWorker::$scheduler->del($this->id,$type);
     }
 
 }
