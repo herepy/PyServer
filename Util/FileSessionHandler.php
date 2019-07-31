cefb2e2d44a1e6549dcc047ef55dac812a66d72a
pengyu <917103429@qq.com>
2019-07-31 10:44:00 +0800

    文件sessionHandler编写

---
+++ Util/FileSessionHandler.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengyu
+ * Date: 2019/7/31
+ * Time: 10:06
+ */
+
+namespace PyServer\Util;
+
+class FileSessionHandler implements \SessionHandlerInterface
+{
+    protected $dir;
+
+    public function __construct($dir)
+    {
+        $this->dir=$dir;
+    }
+
+    public function open($save_path, $name)
+    {
+        if (!file_exists($this->dir)) {
+            mkdir($this->dir,0777,true);
+        }
+        return true;
+    }
+
+    public function close()
+    {
+        return true;
+    }
+
+    public function gc($maxlifetime)
+    {
+        $handle = opendir($this->dir);
+        if ($handle === false) {
+            return 1;
+        }
+
+        $nowTime=time();
+        while (false !== ($file = readdir($handle))) {
+            if ($file == "." || $file == "..") {
+                continue;
+            }
+
+            $filename=$this->dir.DIRECTORY_SEPARATOR.$file;
+            if ((filemtime($filename) + $maxlifetime) < $nowTime) {
+                unlink($filename);
+            }
+        }
+        closedir($handle);
+        return 0;
+    }
+
+    public function read($session_id)
+    {
+        $filename=$this->dir.DIRECTORY_SEPARATOR.$session_id;
+        if (!file_exists($filename)) {
+            return "";
+        }
+
+        return file_get_contents($filename);
+    }
+
+    public function write($session_id, $session_data)
+    {
+        $filename=$this->dir.DIRECTORY_SEPARATOR.$session_id;
+        return file_put_contents($filename,serialize($session_data)) > 0;
+    }
+
+    public function destroy($session_id)
+    {
+        $filename=$this->dir.DIRECTORY_SEPARATOR.$session_id;
+        if (!file_exists($filename)) {
+            return 1;
+        }
+
+        unlink($filename);
+        return 0;
+    }
+
+}
