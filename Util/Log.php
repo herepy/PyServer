4f59ce8fef0324bc9d24dc223e8285eb08a84f71
pengyu <917103429@qq.com>
2019-08-05 15:13:25 +0800

    log分等级

--- Util/Log.php
+++ Util/Log.php
@@ -12,8 +12,15 @@ class Log
 {
     protected static $file;
 
+    protected static $level=["debug","info","notice","warning","error","critical","alert","emergency"];
+
     public static function write($msg,$level="info")
     {
+        if (!in_array($level,self::$level)) {
+            self::write("log level {$level} not allowed");
+            $level="info";
+        }
+
         $content=date("Y/m/d H:i:s")."  [{$level}] ".$msg.PHP_EOL;
         file_put_contents(self::$file,$content,FILE_APPEND );
     }
@@ -22,5 +29,46 @@ class Log
     {
         self::$file=$filename;
     }
+
+    public static function debug($msg)
+    {
+        self::write($msg,"debug");
+    }
+
+    public static function info($msg)
+    {
+        self::write($msg);
+    }
+
+    public static function notice($msg)
+    {
+        self::write($msg,"notice");
+    }
+
+    public static function warning($msg)
+    {
+        self::write($msg,"warning");
+    }
+
+    public static function error($msg)
+    {
+        self::write($msg,"error");
+    }
+
+    public static function critical($msg)
+    {
+        self::write($msg,"critical");
+    }
+
+    public static function alert($msg)
+    {
+        self::write($msg,"alert");
+    }
+
+    public static function emergency($msg)
+    {
+        self::write($msg,"emergency");
+    }
+
 
 }
