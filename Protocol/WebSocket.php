ba09f903c3b6a73135ccbbb1c27934fb369a9903
pengyu <917103429@qq.com>
2019-08-05 11:28:01 +0800

    websocket起头

---
+++ Protocol/WebSocket.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengyu
+ * Date: 2019/8/5
+ * Time: 11:26
+ */
+
+namespace PyServer\Protocol;
+
+class WebSocket implements ProtocolInterface
+{
+    public static function size($buffer)
+    {
+        // TODO: Implement size() method.
+    }
+
+    public static function decode($buffer)
+    {
+        // TODO: Implement decode() method.
+    }
+
+    public static function encode($content)
+    {
+        // TODO: Implement encode() method.
+    }
+}
