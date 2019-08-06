ce4c2daff018fc0e6fd1a29f94f6f81b72aeea6e
pengyu <917103429@qq.com>
2019-08-06 17:58:21 +0800

    新增close操作，写文件时，文件资源句柄一直打开知道进程结束

--- Util/Log.php
+++ Util/Log.php
@@ -13,6 +13,9 @@ class Log
     protected static $logFile;
     protected static $accessFile;
 
+    protected static $logFileHandle;
+    protected static $accessFileHandle;
+
     protected static $level=["debug","info","notice","warning","error","critical","alert","emergency"];
 
     protected static function write($msg,$level="info")
@@ -22,8 +25,12 @@ class Log
             $level="info";
         }
 
+        if (!is_resource(self::$logFileHandle)) {
+            self::$logFileHandle=fopen(self::$logFile,"a");
+        }
+
         $content=date("Y/m/d H:i:s")."  [{$level}] ".$msg.PHP_EOL;
-        file_put_contents(self::$logFile,$content,FILE_APPEND );
+        fwrite(self::$logFileHandle,$content);
     }
 
     public static function setFile($logFile,$accessFile)
@@ -34,11 +41,15 @@ class Log
 
     public static function access($info)
     {
+        if (!is_resource(self::$accessFileHandle)) {
+            self::$accessFileHandle=fopen(self::$accessFile,"a");
+        }
+
         //clientIp - - [date] "method uri HTTP/1.1" httpCode httpSize "referer" "clientAgent"
         $content=$info["ip"].' - - ['.date("Y/m/d H:i:s").'] "'.$info["method"].' '.$info["uri"].' '.$info["protocol"].'" '.
             $info["code"].' '.$info["size"].' "'.$info["referfer"].'" "'.$info["client"].'"'.PHP_EOL;
 
-        file_put_contents(self::$accessFile,$content,FILE_APPEND);
+        fwrite(self::$accessFileHandle,$content);
     }
 
     public static function debug($msg)
@@ -79,6 +90,17 @@ class Log
     public static function emergency($msg)
     {
         self::write($msg,"emergency");
+    }
+
+    public static function close()
+    {
+        if (is_resource(self::$logFileHandle)) {
+            fclose(self::$logFileHandle);
+        }
+
+        if (is_resource(self::$accessFileHandle)) {
+            fclose(self::$accessFileHandle);
+        }
     }
 
 
