8e1653f350d2426c62201a242192bba501db4de3
pengyu <917103429@qq.com>
2019-08-19 09:32:28 +0800

    应用层协议接口增加传输层实例参数

--- src/Protocol/ProtocolInterface.php
+++ src/Protocol/ProtocolInterface.php
@@ -8,27 +8,32 @@
 
 namespace Pengyu\Server\Protocol;
 
+use Pengyu\Server\Transport\TransportInterface;
+
 interface ProtocolInterface
 {
     /**
      * 获取内容长度大小
      * @param string $buffer 内容
+     * @param TransportInterface $connection 内容
      * @return int 大小
      */
-    public static function size($buffer);
+    public static function size($buffer,TransportInterface $connection);
 
     /**
      * 解析内容数据
      * @param string $buffer 要解析的数据
+     * @param TransportInterface $connection 内容
      * @return mixed 解析后的数据
      */
-    public static function decode($buffer);
+    public static function decode($buffer,TransportInterface $connection);
 
     /**
      * 编码内容
      * @param string $content 要编码的内容
+     * @param TransportInterface $connection 内容
      * @return string 编码后的内容
      */
-    public static function encode($content);
+    public static function encode($content,TransportInterface $connection);
 
 }
